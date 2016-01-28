18839b5430e025e839d21fce96a2b1d11250746c
root <root@landregistry.box>
2016-01-28 17:38:48 +0000

    added cname to init.pp in manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,7 @@
 class dnsserver (
   $dns_zone = undef,
   $dns_a_record = undef,
+  $dns_cname_record = undef,
   $dns_forwarders = [ '8.8.8.8', '8.8.4.4' ],
   ){
 
@@ -25,6 +26,10 @@ class dnsserver (
 
   if $dns_a_record {
     create_resources('dns::record::a', $dns_a_record)
+  }
+
+  if $dns_cname_record {
+    create_resources('dns::record::cname', $dns_cname_record)
   }
 
 }
